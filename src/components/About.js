3c1fd3b1acd799197c38a1849de75cc09e2ebcc3
Daniel <danielgonzalez252@gmail.com>
2023-06-14 03:39:58 -0400

    Lab Created

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+function About()
+{
+  return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>Lorem Ipsum</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"/>
+      </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from 'react';
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
-}
+const Home = () => {
+  const name = 'Your Name';
+  const city = 'Your City';
+
+  return (
+      <div id="home">
+        <h1 style={{ color: 'firebrick' }}>{`${name} is a Web Developer from ${city}`}</h1>
+      </div>
+  );
+};
 
 export default Home;
